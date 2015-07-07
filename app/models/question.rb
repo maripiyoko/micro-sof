fcfc19bec346d7b772ce2860e27f545a7a7e800f
Mariko <maripiyok@gmail.com>
2015-07-08 07:51:15 +0900

    Questionのindexで表示する質問本文の、一覧表示用ゲッターを追加。

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,14 @@ class Question < ActiveRecord::Base
   validates :body, presence: true
   validates :user, presence: true
 
+  BODY_SUMMARY_LENGTH = 50
+
+  def summary
+    if self.body && self.body.size > BODY_SUMMARY_LENGTH
+      self.body.slice(0..BODY_SUMMARY_LENGTH) + ".."
+    end
+  end
+
   def created_by?(user)
     return false unless user
     self.user.id == user.id
