9fbbfcc84c50f2ee9c7ef3589cdd6fd7c06026f8
Mariko <maripiyok@gmail.com>
2015-07-09 08:20:56 +0900

    bundle exec annotate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@
 # Table name: answers
 #
 #  id          :integer          not null, primary key
-#  comment     :text(65535)
+#  body        :text(65535)
 #  user_id     :integer
 #  question_id :integer
 #  created_at  :datetime         not null
