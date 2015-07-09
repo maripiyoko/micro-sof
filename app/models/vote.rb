c4e2beffc214b29783dddce38de10d99825fb0a9
Mariko <maripiyok@gmail.com>
2015-07-09 15:50:42 +0900

    Annotate

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  votable_id   :integer
+#  votable_type :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  score        :integer          default(0)
+#
+
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
