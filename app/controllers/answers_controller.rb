778384980e54f581ed9111732819541e94ce04b5
Mariko <maripiyok@gmail.com>
2015-07-09 09:22:10 +0900

    回答を新規作成出来るようにした。
    新規作成時に、ログインユーザーと回答を紐づける！

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,17 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_question
   before_action :set_answer, only: [ :update, :destroy ]
 
   def create
+    @answer = @question.answers.build(answer_params)
+    @answer.user = current_user
+    if @answer.save
+      redirect_to @question, notice: '回答が追加されました。'
+    else
+      redirect_to @question, alert: '回答が追加できませんでした。'
+    end
+
   end
 
   def update
@@ -16,14 +25,18 @@ class AnswersController < ApplicationController
   private
 
     def set_question
-      @question = Questions.find(params[:question_id])
+      @question = Question.find(params[:question_id])
     end
 
     def set_answer
       @answer = @question.find(params[:id])
       unless (@answer.created_by?(current_user))
-        redirect_to @question, alert: '自分の回答ではありません。操作できません。'
+        redirect_to @question, alert: '自分の回答ではないので、操作できません。'
       end
+    end
+
+    def answer_params
+      params.require(:answer).permit(:body)
     end
 
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  let(:login_user) { FactoryGirl.create(:user) }
+  let(:question) { FactoryGirl.create(:question) }
+
+  describe "POST#create" do
+    before { sign_in login_user }
+
+    it "質問に回答が追加出来ること" do
+      new_answer = FactoryGirl.build(:answer, question: question)
+      expect {
+        post :create, { question_id: new_answer.question_id, answer: new_answer.attributes }
+      }.to change(Answer, :count).by(1)
+    end
+
+  end
+
 end
