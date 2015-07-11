c8b1368e602d767dab751fc3c8140d4b32ff8a21
Mariko <maripiyok@gmail.com>
2015-07-12 07:15:44 +0900

    Annotated.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  body             :text(65535)
+#  user_id          :integer
+#  commentable_id   :integer
+#  commentable_type :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
