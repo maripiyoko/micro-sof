64a094177c4f920ff9733efdd54acc4f08c4a935
Mariko <maripiyok@gmail.com>
2015-07-12 07:43:44 +0900

    votes controllerで使っている変数名がぼんやりしてたので、リネーム

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,22 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_target
+  before_action :set_vote
 
   def up
-    @target.increment(:score)
-    if @target.valid?
-      @target.save!
-      render json: @target.votable.sum_votes
+    @vote.increment(:score)
+    if @vote.valid?
+      @vote.save!
+      render json: @vote.votable.sum_votes
     else
       render json: 'you are already voted.', status: :unprocessable_entity
     end
   end
 
   def down
-    @target.decrement(:score)
-    if @target.valid?
-      @target.save!
-      render json: @target.votable.sum_votes
+    @vote.decrement(:score)
+    if @vote.valid?
+      @vote.save!
+      render json: @vote.votable.sum_votes
     else
       render json: 'you are already voted.', status: :unprocessable_entity
     end
@@ -25,14 +25,14 @@ class VotesController < ApplicationController
 
   private
 
-    def set_target
+    def set_vote
       if params[:question_id]
         # question_id がある場合、idはanswer
         answer = Answer.find(params[:id])
-        @target = answer.votes.find_or_initialize_by(user_id: current_user.id)
+        @vote = answer.votes.find_or_initialize_by(user_id: current_user.id)
       else
         question = Question.find(params[:id])
-        @target = question.votes.find_or_initialize_by(user_id: current_user.id)
+        @vote = question.votes.find_or_initialize_by(user_id: current_user.id)
       end
     end
 
