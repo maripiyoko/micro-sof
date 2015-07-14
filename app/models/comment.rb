9bbc201a506d5d59343eb5648cb4d25974a01a65
Mariko <maripiyok@gmail.com>
2015-07-15 07:50:38 +0900

    回答、コメントが付いた時に、質問の更新日も更新。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,7 +13,7 @@
 
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :user, presence: true
   validates :commentable, presence: true
