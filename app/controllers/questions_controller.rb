1c03d5fc15fa1b6991d779bf01509565ec100a72
Mariko <maripiyok@gmail.com>
2015-07-15 07:44:43 +0900

    質問一覧表示時に、更新日の新しいもの順にするように変更。

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   before_action :set_own_question, only: [:edit, :update, :destroy]
 
   def index
-    @questions = Question.all.page(params[:page])
+    @questions = Question.order(updated_at: :desc).page(params[:page])
   end
 
   def show
