2e6e083dfd1f7110a49d4d80915aa88ecfc3f40e
Mariko <maripiyok@gmail.com>
2015-07-22 16:55:46 +0900

    Add question model spec.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -15,5 +15,27 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "有効なファクトリをもつこと" do
+    expect(FactoryGirl.build(:question)).to be_valid
+  end
+
+  it "titleがなければ、無効な状態であること" do
+    question = FactoryGirl.build(:question, title: nil)
+    question.valid?
+    expect(question.errors[:title]).to include("can't be blank")
+  end
+
+  it "bodyがなければ、無効な状態であること" do
+    question = FactoryGirl.build(:question, body: nil)
+    question.valid?
+    expect(question.errors[:body]).to include("can't be blank")
+  end
+
+  it "titleとbodyがあれば、有効な状態であること" do
+    question = FactoryGirl.build(:question, title: 'MY QUESTION', body: 'MY BODY')
+    question.valid?
+    expect(question).to be_valid
+  end
+
 end
